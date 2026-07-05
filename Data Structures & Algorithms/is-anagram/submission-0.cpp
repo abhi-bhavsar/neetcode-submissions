da5a7dd34eb155ef3788b49cb3f5ff73a24eca80
Abhishek Bhavsar <163317055+abhi-bhavsar@users.noreply.github.com>
2026-07-05 18:11:21 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()){return false ;}
+        vector<int>v(27,0) ;
+        for(int i=0;i<s.length();i++) {
+            v[s[i]-'a']++ ;
+            v[t[i]-'a']--;
+        }
+        for(int i=0;i<27;i++) {
+            if(v[i]!=0){return false ;}
+        }
+        return true;
+    }
+};
