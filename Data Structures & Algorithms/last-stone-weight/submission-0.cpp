86024b9b05c659e42ea668df9aa497789a03b0e4
Abhishek Bhavsar <163317055+abhi-bhavsar@users.noreply.github.com>
2026-07-11 07:50:30 +0530

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>pq ;
+        for(int i=0;i<stones.size();i++) {
+            pq.push(stones[i]) ;
+        }
+
+        while(pq.size()!=1){
+            int num1 = pq.top() ;
+            pq.pop() ;
+            int num2 = pq.top() ;
+            pq.pop() ;
+            pq.push(num1-num2) ;
+        }
+        return pq.top() ;
+    }
+};
